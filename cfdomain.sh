7a993e19d2581edcac3aa9bedbae5892ce161d5f
takbebeh <131166147+takbebeh@users.noreply.github.com>
2023-06-04 21:11:54 +0700

    Add files via upload

---
+++ cfdomain.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+DOMAIN=alfvpn.my.id
+sub=$(tr </dev/urandom -dc a-z0-9 | head -c3)
+SUB_DOMAIN=${sub}.alfvpn.my.id
+CF_ID=alfinproject22@gmail.com
+CF_KEY=a9d92b093d290b0a16ed6d5610f4c6baa667d
+set -euo pipefail
+IP=$(wget -qO- ipinfo.io/ip)
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","proxied":false}')
+echo $SUB_DOMAIN >/etc/xray/domain
+echo "Subdomain kamu adalah : $SUB_DOMAIN"
+sleep 3
+rm -f /root/cfdomain.sh
